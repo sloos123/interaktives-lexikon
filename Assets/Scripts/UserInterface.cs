cd0355036fe733d451d16a907a29c1da78152b64
Sarah Loos <s.loos@hotmail.com>
2019-01-15 16:11:31 +0100

    Updated buttons

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -5,13 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class UserInterface : MonoBehaviour {
 
+    public GameObject musicButton;
+    public GameObject soundButton;
+    public GameObject settingsBackground;
+
+    public bool statusSettings = false;
+
 	// Use this for initialization
 	void Start () {
-		
+        musicButton = GameObject.Find("Music button");
+        soundButton = GameObject.Find("Sound button");
+        settingsBackground = GameObject.Find("Settings background");
+
+        musicButton.SetActive(false);
+        soundButton.SetActive(false);
+        settingsBackground.SetActive(false);
 	}
 
     public void StartGame () {
         SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+
+    public void Settings () {
+        if (statusSettings == false){
+            musicButton.SetActive(true);
+            soundButton.SetActive(true);
+            settingsBackground.SetActive(true);
+
+            statusSettings = true;
+        } else {
+            musicButton.SetActive(false);
+            soundButton.SetActive(false);
+            settingsBackground.SetActive(false);
+
+            statusSettings = false;
+        }
     }
 	
 	// Update is called once per frame
