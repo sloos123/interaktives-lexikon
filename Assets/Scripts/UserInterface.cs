5ec4161812ce3d5e483592439120600071fe3900
Sarah Loos <s.loos@hotmail.com>
2019-01-15 15:09:39 +0100

    Updated user interface

---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UserInterface : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void StartGame () {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
