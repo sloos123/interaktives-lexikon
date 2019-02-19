93932c6c1e62dbed70f30a2794bb4d0af4e7eb5e
sloos123 <43366113+sloos123@users.noreply.github.com>
2019-02-19 15:42:25 +0100

    Add support for augmented images

---
+++ Assets/Scripts/AugmentedImageController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+
+public class AugmentedImageController : MonoBehaviour
+{
+    public AugmentedImageVisualizer AugmentedImageVisualizerPrefab;
+    public GameObject FitToScanOverlay;
+
+    private Dictionary<int, AugmentedImageVisualizer> m_Visualizers = new Dictionary<int, AugmentedImageVisualizer>();
+    private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
+
+        foreach (var image in m_TempAugmentedImages)
+        {
+            AugmentedImageVisualizer visualizer = null;
+            m_Visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+
+            if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+            {
+                Anchor anchor = image.CreateAnchor(image.CenterPose);
+                visualizer = (AugmentedImageVisualizer)Instantiate(AugmentedImageVisualizerPrefab, anchor.transform);
+                visualizer.Image = image;
+                m_Visualizers.Add(image.DatabaseIndex, visualizer);
+            }
+            else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+            {
+                m_Visualizers.Remove(image.DatabaseIndex);
+                GameObject.Destroy(visualizer.gameObject);
+            }
+        }
+
+        foreach (var visualizer in m_Visualizers.Values)
+        {
+            if (visualizer.Image.TrackingState == TrackingState.Tracking)
+            {
+                FitToScanOverlay.SetActive(false);
+                return;
+            }
+        }
+
+        FitToScanOverlay.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/AugmentedImageVisualizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+
+public class AugmentedImageVisualizer : MonoBehaviour
+{
+    public AugmentedImage Image;
+
+    public GameObject TestObject;
+
+    void Update()
+    {
+        if (Image == null || Image.TrackingState != TrackingState.Tracking)
+        {
+            TestObject.SetActive(false);
+        }
+
+        TestObject.SetActive(true);
+    }
+}
